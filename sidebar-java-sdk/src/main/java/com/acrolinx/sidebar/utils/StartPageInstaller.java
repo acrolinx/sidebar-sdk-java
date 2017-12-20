0468f74f994bfba4a55e062c2aefe3df55f1adb5
franziska.preiss <franziska.preiss@acrolinx.com>
2017-12-20 13:03:31 +0100

    DEV-15153 Make sure several instances can run at the same time
    * new release version
    * updated javadocs

--- sidebar-java-sdk/src/main/java/com/acrolinx/sidebar/utils/StartPageInstaller.java
+++ sidebar-java-sdk/src/main/java/com/acrolinx/sidebar/utils/StartPageInstaller.java
@@ -1,18 +1,20 @@
 
 package com.acrolinx.sidebar.utils;
 
-import static org.eclipse.swt.internal.Platform.lock;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
-import java.nio.channels.OverlappingFileLockException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.Properties;
 
-import org.eclipse.swt.internal.Platform;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
