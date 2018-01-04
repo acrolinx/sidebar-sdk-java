ca980c6a16592053630285f12d9cd934f3bfbbc3
franziska.preiss <franziska.preiss@acrolinx.com>
2018-01-04 18:00:58 +0100

    DEV-14835 Preselect file in folder when opening log file location
    * added functionality for mac os and windows
    * updated javadoc

---
+++ sidebar-java-sdk/src/main/java/com/acrolinx/sidebar/utils/OSUtils.java
@@ -0,0 +1,47 @@
+
+package com.acrolinx.sidebar.utils;
+
+/**
+ * Retrieves the current OS.
+ */
+
+public class OSUtils
+{
+    public static enum EnumOS
+    {
+        linux, macos, unknown, windows;
+
+        public boolean isLinux()
+        {
+
+            return this == linux;
+        }
+
+        public boolean isMac()
+        {
+
+            return this == macos;
+        }
+
+        public boolean isWindows()
+        {
+
+            return this == windows;
+        }
+    }
+
+    public static EnumOS getOS()
+    {
+        String s = System.getProperty("os.name").toLowerCase();
+
+        if (s.contains("mac"))
+            return EnumOS.macos;
+        else if (s.contains("win"))
+            return EnumOS.windows;
+        else if (s.contains("linux") || s.contains("sunos") || s.contains("solaris") || s.contains("unix"))
+            return EnumOS.linux;
+        else
+            return EnumOS.unknown;
+    }
+
+}
