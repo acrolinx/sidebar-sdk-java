4d05a8d0fe8c19145658acb81b72613c372e6fba
franziska.preiss <franziska.preiss@acrolinx.com>
2018-01-11 19:05:14 +0100

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-STAGE = $1
+STAGE=$1
+
+echo "$1"
 
 GRADLE_PROPERTIES_FILE=gradle.properties
 
-function getProperty {
+is_not_substring(){
+    case "$2" in
+        *$1*) return 1;;
+        *) return 0;;
+    esac
+}
+
+getProperty()
+{
     PROP_KEY=$1
     PROP_VALUE=`cat $GRADLE_PROPERTIES_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
     echo $PROP_VALUE
 }
+
 PROJECT_VERSION=$(getProperty "currentVersion")
 
-if [ "$(STAGE)" == 'snapshot' ]; then
+if [ "$STAGE" = "snapshot" ]; then
         ./gradlew pP publish
         exit 1;
 fi
 
-if [ "$(STAGE)" == 'release' ]; then
+if [ "$STAGE" = "release" ]; then
         ./gradlew pP publish
-        if [ "$(STAGE)" == 'release' && "$(PROJECT_VERSION)" != *"$SNAPSHOT"*]; then
+        if [ "$STAGE" = 'release' ] && is_not_substring "$SNAPSHOT" "$PROJECT_VERSION"; then
             ./gradlew pP publish publishDocsAndCreateGithubReleaseTag closeRepository
         fi
         exit 1;
