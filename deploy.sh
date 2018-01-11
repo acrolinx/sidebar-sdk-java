fb261a35526275e318a8c19b547f5a26d22770aa
franziska.preiss <franziska.preiss@acrolinx.com>
2018-01-11 17:51:50 +0100

    Set up deployment and build scripts

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,24 @@
 
 STAGE = $1
 
-if [ "$(STAGE)" == 'develop' ]; then
+GRADLE_PROPERTIES_FILE=gradle.properties
+
+function getProperty {
+    PROP_KEY=$1
+    PROP_VALUE=`cat $GRADLE_PROPERTIES_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+    echo $PROP_VALUE
+}
+PROJECT_VERSION=$(getProperty "currentVersion")
+
+if [ "$(STAGE)" == 'snapshot' ]; then
         ./gradlew pP publish
         exit 1;
 fi
 
-if [ "$(STAGE)" == 'production' ]; then
-        ./gradlew pP publish pP publish publishDocsAndCreateGithubReleaseTag closeRepostitory
+if [ "$(STAGE)" == 'release' ]; then
+        ./gradlew pP publish
+        if [ "$(STAGE)" == 'release' && "$(PROJECT_VERSION)" != *"$SNAPSHOT"*]; then
+            ./gradlew pP publish publishDocsAndCreateGithubReleaseTag closeRepository
+        fi
         exit 1;
 fi
