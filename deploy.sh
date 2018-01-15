3d682ddab34a0afdb1f5caac13d75fe4560ae2c0
franziska.preiss <franziska.preiss@acrolinx.com>
2018-01-15 14:56:05 +0100

    Releasing sidebar sdk

--- deploy.sh
+++ deploy.sh
@@ -36,6 +36,9 @@ if [ "$STAGE" = "release" ]; then
                     exit 0
                 else
                     exit 1
+                fi
+            else
+                exit 1
             fi
         else
            exit 1
