7317556f7ba06444fa68b501636e4293dc040e1e
franziska.preiss <franziska.preiss@acrolinx.com>
2018-01-15 16:45:45 +0100

    Releasing sidebar sdk

--- deploy.sh
+++ deploy.sh
@@ -2,25 +2,27 @@
 
 STAGE=$1
 
-echo "$1"
+echo "Stage: $1"
 
 GRADLE_PROPERTIES_FILE=gradle.properties
 
 is_not_substring(){
-    case "$2" in
-        *$1*) return 1;;
-        *) return 0;;
-    esac
+     if [ -z "${2##*$1*}" ]; then
+            return 1
+        else return 0
+     fi
 }
 
 getProperty()
 {
     PROP_KEY=$1
-    PROP_VALUE=`cat $GRADLE_PROPERTIES_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+    PROP_VALUE=`cat "./gradle.properties" | grep "$PROP_KEY" | cut -d'=' -f2`
     echo $PROP_VALUE
 }
 
+
 PROJECT_VERSION=$(getProperty "currentVersion")
+echo "Current Version: $PROJECT_VERSION"
 
 if [ "$STAGE" = "snapshot" ]; then
     if ./gradlew pP publish; then
