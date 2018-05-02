acb00d5ae1a35bf9634d31984eeff929cc6bbf2c
franziska.preiss <franziska.preiss@acrolinx.com>
2018-05-02 19:04:41 +0200

    Spotless

--- sidebar-java-sdk/src/main/java/com/acrolinx/sidebar/jfx/AcrolinxSidebarPlugin.java
+++ sidebar-java-sdk/src/main/java/com/acrolinx/sidebar/jfx/AcrolinxSidebarPlugin.java
@@ -12,6 +12,17 @@
 
 package com.acrolinx.sidebar.jfx;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+import javafx.application.Platform;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.acrolinx.sidebar.AcrolinxIntegration;
 import com.acrolinx.sidebar.pojo.SidebarError;
 import com.acrolinx.sidebar.pojo.document.*;
@@ -19,22 +30,14 @@ import com.acrolinx.sidebar.pojo.settings.*;
 import com.acrolinx.sidebar.utils.LogMessages;
 import com.acrolinx.sidebar.utils.SidebarUtils;
 import com.google.common.base.Preconditions;
-import javafx.application.Platform;
-import netscape.javascript.JSObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
+import netscape.javascript.JSObject;
 
 /**
  * For internal use.
  */
-@SuppressWarnings({"unused", "WeakerAccess"}) abstract class AcrolinxSidebarPlugin
+@SuppressWarnings({"unused", "WeakerAccess"})
+abstract class AcrolinxSidebarPlugin
 {
     final AcrolinxIntegration client;
     private final JSObject jsobj;
@@ -134,10 +137,10 @@ import java.util.stream.Collectors;
 
     public void invalidateRangesForMatches(List<? extends AbstractMatch> matches)
     {
-        List<CheckedDocumentPart> invalidDocumentParts = matches.stream().map(
-                (match) -> new CheckedDocumentPart(currentCheckId.get(),
-                        new IntRange(match.getRange().getMinimumInteger(),
-                                match.getRange().getMaximumInteger()))).collect(Collectors.toList());
+        List<CheckedDocumentPart> invalidDocumentParts = matches.stream().map((match) -> new CheckedDocumentPart(
+                currentCheckId.get(),
+                new IntRange(match.getRange().getMinimumInteger(), match.getRange().getMaximumInteger()))).collect(
+                        Collectors.toList());
         invalidateRanges(invalidDocumentParts);
     }
 
