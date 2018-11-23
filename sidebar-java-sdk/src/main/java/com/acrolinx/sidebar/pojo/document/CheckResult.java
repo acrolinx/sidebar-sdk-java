52cae836834e34df83971c54a0ebb6fd16e3075f
Ralf Kühnlein <ralf.kuehnlein@acrolinx.com>
2018-11-23 12:18:27 +0100

    DEV-19227 made order of embedded check information stable

--- sidebar-java-sdk/src/main/java/com/acrolinx/sidebar/pojo/document/CheckResult.java
+++ sidebar-java-sdk/src/main/java/com/acrolinx/sidebar/pojo/document/CheckResult.java
@@ -2,7 +2,7 @@
 
 package com.acrolinx.sidebar.pojo.document;
 
-import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import com.acrolinx.sidebar.pojo.settings.InputFormat;
@@ -11,18 +11,18 @@ import com.acrolinx.sidebar.pojo.settings.InputFormat;
 public class CheckResult
 {
     private final CheckedDocumentPart checkedDocumentPart;
-    private HashMap<String, String> embedCheckInformation;
+    private Map<String, String> embedCheckInformation;
     public final static String ACROLINX_PROCESSING_INSTRUCTION_TAG_NAME = "acrolinxCheckData";
     private String inputFormat;
 
-    public CheckResult(CheckedDocumentPart checkedDocumentPart)
+    public CheckResult(final CheckedDocumentPart checkedDocumentPart)
     {
         this.checkedDocumentPart = checkedDocumentPart;
     }
 
     @SuppressWarnings("UnusedParameters")
-    public CheckResult(CheckedDocumentPart checkedDocumentPart, HashMap<String, String> embedCheckInformation,
-            String inputFormat)
+    public CheckResult(final CheckedDocumentPart checkedDocumentPart, final Map<String, String> embedCheckInformation,
+            final String inputFormat)
     {
         this.checkedDocumentPart = checkedDocumentPart;
         this.embedCheckInformation = embedCheckInformation;
@@ -34,7 +34,7 @@ public class CheckResult
         return checkedDocumentPart;
     }
 
-    public Optional<HashMap<String, String>> getEmbedCheckInformation()
+    public Optional<Map<String, String>> getEmbedCheckInformation()
     {
         return Optional.ofNullable(embedCheckInformation);
     }
@@ -42,7 +42,7 @@ public class CheckResult
     public Optional<String> getEmbedCheckDataAsEmbeddableString()
     {
         if (InputFormat.XML.toString().equalsIgnoreCase(this.inputFormat)) {
-            StringBuilder sb = new StringBuilder();
+            final StringBuilder sb = new StringBuilder();
             getEmbedCheckInformation().ifPresent((map) -> {
                 sb.append("<?" + ACROLINX_PROCESSING_INSTRUCTION_TAG_NAME + " ");
                 map.forEach((key, value) -> sb.append(key).append("=").append("\"").append(value).append("\" "));
@@ -52,7 +52,7 @@ public class CheckResult
                 return Optional.of(sb.toString());
             }
         } else if (InputFormat.MARKDOWN.toString().equalsIgnoreCase(this.inputFormat)) {
-            StringBuilder sb = new StringBuilder();
+            final StringBuilder sb = new StringBuilder();
             getEmbedCheckInformation().ifPresent((map) -> {
                 sb.append("<!-- " + ACROLINX_PROCESSING_INSTRUCTION_TAG_NAME + " ");
                 map.forEach((key, value) -> sb.append(key).append("=").append("\"").append(value).append("\" "));
@@ -62,7 +62,7 @@ public class CheckResult
                 return Optional.of(sb.toString());
             }
         } else if (InputFormat.HTML.toString().equalsIgnoreCase(this.inputFormat)) {
-            StringBuilder sb = new StringBuilder();
+            final StringBuilder sb = new StringBuilder();
             getEmbedCheckInformation().ifPresent((map) -> {
                 sb.append("<meta " + "name=\"" + ACROLINX_PROCESSING_INSTRUCTION_TAG_NAME + "\" ");
                 map.forEach((key, value) -> sb.append(key).append("=").append("\"").append(value).append("\" "));
