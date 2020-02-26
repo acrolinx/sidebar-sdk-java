252b58876ab3b5fd48edbd3d17b4516307ae3898
Franziska Preiß <franziska.preiss@acrolinx.com>
2020-02-26 13:18:27 +0100

    DEV-22685 Making software component id, suitable for coding guidance suggestion.

--- src/main/java/com/acrolinx/sidebar/pojo/settings/AcrolinxSidebarInitParameter.java
+++ src/main/java/com/acrolinx/sidebar/pojo/settings/AcrolinxSidebarInitParameter.java
@@ -42,12 +42,11 @@ public class AcrolinxSidebarInitParameter
         copy.addAll(builder.clientComponents);
         copy.add(SidebarUtils.getJavaSDKSoftwareComponent());
 
-        String osID = System.getProperty("os.name");
-
-        String javaID = ("java.runtime" + " " +
+        String osID = ("os." + System.getProperty("os.name").replaceAll(" ", "."));
+        String javaID = ("java.runtime." +
                 System.getProperty("java.vendor")).replaceAll(" ", ".");
 
-        copy.add(new SoftwareComponent(osID.replaceAll(" ", "."), osID, System.getProperty("os.version"),
+        copy.add(new SoftwareComponent(osID, System.getProperty("os.name"), System.getProperty("os.version"),
                 SoftwareComponentCategory.DETAIL));
         copy.add(new SoftwareComponent(javaID, System.getProperty("java.runtime.name"),
                 System.getProperty("java.runtime.version"), SoftwareComponentCategory.DETAIL));
