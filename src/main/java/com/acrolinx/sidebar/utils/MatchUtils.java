d1ba6248a0d2dd7f5e06832e4be21157a02457e6
Justin Horn <justin.horn@acrolinx.com>
2022-06-21 10:23:08 +0200

    DEV-30114; feat: filter xml tag content in external content matches

--- src/main/java/com/acrolinx/sidebar/utils/MatchUtils.java
+++ src/main/java/com/acrolinx/sidebar/utils/MatchUtils.java
@@ -2,11 +2,13 @@
 
 package com.acrolinx.sidebar.utils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-import com.acrolinx.sidebar.pojo.document.AcrolinxMatch;
-import com.acrolinx.sidebar.pojo.document.AcrolinxMatchWithReplacement;
+import com.acrolinx.sidebar.pojo.document.externalContent.ExternalContentField;
+import com.acrolinx.sidebar.pojo.document.externalContent.ExternalContentMatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,22 +20,46 @@ public class MatchUtils
 
     private static final Logger logger = LoggerFactory.getLogger(SidebarUtils.class);
 
+
     public static List<? extends AbstractMatch> filterDangerousMatches(List<? extends AbstractMatch> list,
-            String lastCheckedContent)
+                                                                       String lastCheckedContent) {
+        return filterDangerousMatches(list,lastCheckedContent,new ArrayList<>());
+    }
+
+    public static List<? extends AbstractMatch> filterDangerousMatches(List<? extends AbstractMatch> list,
+                                                                       String lastCheckedContent, List<ExternalContentField> lastCheckedExternalContent)
     {
         return list.stream().filter(m -> {
             //todo: does the external content contain tags that are not external content matches?
-            if ( (m instanceof AcrolinxMatch) && ((AcrolinxMatch) m).hasExternalContentMatches()) return true;
+            if (m.hasExternalContentMatches()) {
+                return isExternalContentMatchAXMLTag(m,lastCheckedExternalContent);
+            }
             String matchContent = lastCheckedContent.substring(m.getRange().getMinimumInteger(),
                     m.getRange().getMaximumInteger());
             logger.debug("Checking if match is a tag");
-
 
             boolean isTag = matchContent.matches("</?\\w+.*?>");
             logger.debug("Is match a tag: " + isTag);
             return !isTag;
 
         }).collect(Collectors.toList());
+    }
+
+    public static boolean isExternalContentMatchAXMLTag(AbstractMatch match, List<ExternalContentField> lastCheckedExternalContent) {
+        ExternalContentMatch externalContentMatch = match.getExternalContentMatches().get(0);
+        while (externalContentMatch.getExternalContentMatches().size() > 0) {
+            externalContentMatch = externalContentMatch.getExternalContentMatches().get(0);
+        }
+        final String externalContentId = externalContentMatch.getId();
+        Optional<ExternalContentField> optionalExternalContentField = lastCheckedExternalContent.stream().filter(externalContentField -> externalContentField.getId().equals(externalContentId)).findFirst();
+        if(!optionalExternalContentField.isPresent()) {
+            logger.warn("Field for externalContentMatch has not been found");
+            return false;
+        }
+        ExternalContentField externalContentField = optionalExternalContentField.get();
+        String content = externalContentField.getContent();
+        String matchContent = content.substring(externalContentMatch.getRange().getMinimumInteger(),externalContentMatch.getRange().getMaximumInteger());
+        return !matchContent.matches("</?\\w+.*?>");
     }
 
 
