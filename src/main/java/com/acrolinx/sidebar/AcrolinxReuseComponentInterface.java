6dbd5a495d31954bac9b74190a6965471dc458ef
Justin Horn <justin.horn@acrolinx.com>
2022-09-22 17:40:20 +0200

    DEV-31042; feat: add message capabilities to ReuseState

--- src/main/java/com/acrolinx/sidebar/AcrolinxReuseComponentInterface.java
+++ src/main/java/com/acrolinx/sidebar/AcrolinxReuseComponentInterface.java
@@ -7,14 +7,6 @@ import com.acrolinx.sidebar.reuse.ReuseState;
 
 
 public interface AcrolinxReuseComponentInterface {
-
-//    public void showPreferredPhrases(List<String> preferredPhrases);
-//
-//    public void showPreferredPhrasesAndOriginal(List<String> preferredPhrases, String original);
-//
-//    public void setLoading(boolean loading,String queriedPhrase);
-//
-//    public void setCurrentSentence(String currentSentence);
     void queryCurrentSentence();
 
     void setReuseState(ReuseState reuseState);
