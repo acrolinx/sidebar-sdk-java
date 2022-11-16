f43799ed4dac176405e4603dbaa861fc11182d66
Justin Horn <justin.horn@acrolinx.com>
2022-11-16 16:21:35 +0100

    DEV-31250 feat: use Locale instead of String for language

--- src/main/java/com/acrolinx/sidebar/reuse/ReusePanelState.java
+++ src/main/java/com/acrolinx/sidebar/reuse/ReusePanelState.java
@@ -7,23 +7,24 @@ import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ReusePanelState {
 
-    public static final ReusePanelState EMPTY_REUSE_PANEL_STATE = new ReusePanelState(ReuseMessage.NO_MESSAGE,new ArrayList<>(),"","",false, "en");
+    public static final ReusePanelState EMPTY_REUSE_PANEL_STATE = new ReusePanelState(ReuseMessage.NO_MESSAGE,new ArrayList<>(),"","",false, Locale.ENGLISH);
     private final ReuseMessage message;
     private final boolean loading;
     private final List<ReuseSuggestion> suggestions;
     private final String searchString;
-    private final String potentialNextSearchString;
-    private final String language;
+    private final String currentString;
+    private final Locale language;
 
-    public ReusePanelState(ReuseMessage message, List<ReuseSuggestion> suggestions, String searchString, String potentialNextSearchString, boolean loading, String language) {
+    public ReusePanelState(ReuseMessage message, List<ReuseSuggestion> suggestions, String searchString, String currentString, boolean loading, Locale language) {
         this.message = message;
         this.loading = loading;
         this.suggestions = suggestions;
         this.searchString = searchString;
-        this.potentialNextSearchString = potentialNextSearchString;
+        this.currentString = currentString;
         this.language = language;
     }
 
@@ -39,8 +40,8 @@ public class ReusePanelState {
         return searchString;
     }
 
-    public String getPotentialNextSearchString() {
-        return potentialNextSearchString;
+    public String getCurrentString() {
+        return currentString;
     }
 
     public ReuseMessage getMessage() {
@@ -51,7 +52,26 @@ public class ReusePanelState {
         return (new Gson()).toJson(this);
     }
 
-    public String getLanguage() {
+    public Locale getLanguage() {
         return language;
+    }
+
+    public ReusePanelState changeMessage(ReuseMessage message) {
+        return new ReusePanelState(message,this.suggestions,this.searchString,this.currentString,this.loading,this.language);
+    }
+    public ReusePanelState changeSuggestions(List<ReuseSuggestion> reuseSuggestions) {
+        return new ReusePanelState(this.message,reuseSuggestions,this.searchString,this.currentString,this.loading,this.language);
+    }
+    public ReusePanelState changeSearchString(String searchString) {
+        return new ReusePanelState(this.message,this.suggestions,searchString,this.currentString,this.loading,this.language);
+    }
+    public ReusePanelState changeCurrentString(String currentString) {
+        return new ReusePanelState(this.message,this.suggestions,this.searchString,currentString,this.loading,this.language);
+    }
+    public ReusePanelState changeLoading(boolean loading) {
+        return new ReusePanelState(this.message,this.suggestions,this.searchString,this.currentString,loading,this.language);
+    }
+    public ReusePanelState changeLanguage(Locale language) {
+        return new ReusePanelState(this.message,this.suggestions,this.searchString,this.currentString,this.loading,language);
     }
 }
