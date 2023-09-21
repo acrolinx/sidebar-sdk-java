27bbf5fef9f8629af7a4c838594cbde21130f6bf
willynganga <willy.mwangi@acrolinx.com>
2023-09-21 10:19:57 +0300

    Refactor method name

--- src/test/java/com/acrolinx/sidebar/pojo/settings/CheckOptionsTest.java
+++ src/test/java/com/acrolinx/sidebar/pojo/settings/CheckOptionsTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 class CheckOptionsTest
 {
     @Test
-    void testToStringHasTheProperKeys()
+    void toStringTest()
     {
         CheckOptions checkOptions = createCheckOptions();
 
