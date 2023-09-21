e3f9f1eaf963bd88f45d45fc072ea23b26bccc60
willynganga <willy.mwangi@acrolinx.com>
2023-09-21 15:37:39 +0300

    Use serialized name for documentSelection field
    This field is required by check selection

--- src/main/java/com/acrolinx/sidebar/pojo/settings/CheckOptions.java
+++ src/main/java/com/acrolinx/sidebar/pojo/settings/CheckOptions.java
@@ -3,6 +3,7 @@ package com.acrolinx.sidebar.pojo.settings;
 
 import com.acrolinx.sidebar.pojo.document.externalcontent.ExternalContent;
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 import javax.annotation.Nullable;
 
 /**
@@ -12,7 +13,8 @@ public class CheckOptions
 {
     private final InputFormat inputFormat;
     private final RequestDescription requestDescription;
-    private final DocumentSelection selection;
+    @SerializedName("selection")
+    private final DocumentSelection documentSelection;
     private final ExternalContent externalContent;
 
     /**
@@ -20,12 +22,12 @@ public class CheckOptions
      * @param requestDescription Contains the document reference. This can be an id or path to identify
      *        the document.
      */
-    public CheckOptions(RequestDescription requestDescription, InputFormat inputFormat, DocumentSelection selection,
-            @Nullable ExternalContent externalContent)
+    public CheckOptions(RequestDescription requestDescription, InputFormat inputFormat,
+            DocumentSelection documentSelection, @Nullable ExternalContent externalContent)
     {
         this.requestDescription = requestDescription;
         this.inputFormat = inputFormat;
-        this.selection = selection;
+        this.documentSelection = documentSelection;
         this.externalContent = externalContent;
     }
 
