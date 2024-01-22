67d1d19dacdc5ccb01c92c9dbdb3db54e83f30cb
willynganga <willy.mwangi@acrolinx.com>
2024-01-22 10:12:24 +0300

    refactor: split build and deploy phases

---
+++ deploy-snapshot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Starting publish script"
+
+getProperty() {
+    PROP_KEY=$1
+    PROP_VALUE=`cat "./gradle.properties" | grep "$PROP_KEY" | cut -d'=' -f2`
+    echo $PROP_VALUE
+}
+
+readonly PROJECT_VERSION=$(getProperty "currentVersion")
+echo "Current Version: $PROJECT_VERSION"
+
+if [[ "$PROJECT_VERSION" == *"SNAPSHOT"* ]]; then
+    echo "Publishing snapshot version to snapshot repo..."
+    if ./gradlew publishToSonatype; then
+        echo "Published snapshot version to snapshot repo..."
+        exit 0
+    else
+        exit 1
+    fi
+fi
