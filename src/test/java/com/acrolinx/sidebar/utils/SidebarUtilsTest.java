0e96943bee8c9409607cc11e50dc8e0d16d5e0be
willynganga <willy.mwangi@acrolinx.com>
2025-03-07 16:42:37 +0300

    Add test

--- src/test/java/com/acrolinx/sidebar/utils/SidebarUtilsTest.java
+++ src/test/java/com/acrolinx/sidebar/utils/SidebarUtilsTest.java
@@ -11,6 +11,24 @@ import org.junit.jupiter.api.Test;
 
 class SidebarUtilsTest {
   @Test
+  void getSidebarErrorHtml() {
+    final String expectedHtml =
+        "<!DOCTYPE html>\n"
+            + "<html lang=\"en\">\n"
+            + "<head>\n"
+            + "  <meta charset=\"UTF-8\">\n"
+            + "  <title>Error while trying to load sidebar</title>\n"
+            + "</head>\n"
+            + "<body>\n"
+            + "Sidebar start page failed to load. Please check log files: \n"
+            + "logs.xml\n"
+            + "</body>\n"
+            + "</html>";
+
+    Assertions.assertEquals(expectedHtml, SidebarUtils.getSidebarErrorHtml("logs.xml"));
+  }
+
+  @Test
   void isValidUrlNullTest() {
     assertFalse(SidebarUtils.isValidUrl(null));
   }
