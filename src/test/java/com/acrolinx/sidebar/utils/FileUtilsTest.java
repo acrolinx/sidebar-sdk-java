d5a90c4f611cf0ab0f83d2e5dcd17bf3c6ea67cc
willynganga <willy.mwangi@acrolinx.com>
2025-03-17 16:50:47 +0300

    Code clean up

--- src/test/java/com/acrolinx/sidebar/utils/FileUtilsTest.java
+++ src/test/java/com/acrolinx/sidebar/utils/FileUtilsTest.java
@@ -1,8 +1,6 @@
 /* Copyright (c) 2025 Acrolinx GmbH */
 package com.acrolinx.sidebar.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
@@ -10,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class FileUtilsTest {
 
   @Test
-  void extractZipFile() throws Exception {
+  void extractZipFileTest() throws Exception {
     Path zipFilePath = Path.of("src/test/resources/sidebar-startpage.zip");
     Path tempDirectory = Files.createTempDirectory(null);
 
