086f4b86bee0e944a257046c1c81e7b3e68752b7
willynganga <willy.mwangi@acrolinx.com>
2025-03-24 15:05:08 +0300

    Check if directory exists

--- src/main/java/com/acrolinx/sidebar/utils/FileUtils.java
+++ src/main/java/com/acrolinx/sidebar/utils/FileUtils.java
@@ -21,6 +21,10 @@ public final class FileUtils {
   private static final Logger logger = LoggerFactory.getLogger(FileUtils.class);
 
   public static void deleteDirectory(Path directory) throws IOException {
+    if (!Files.exists(directory)) {
+      return;
+    }
+
     Validate.isDirectory(directory, "directory");
 
     try (Stream<Path> stream = Files.walk(directory)) {
